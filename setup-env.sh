e1b4c6eda34a797582e88c28047ff7ffec5b324d
Yehuda Korotkin <yehuda@alefbt.com>
2021-02-03 12:52:08 +0200

    more majic

--- setup-env.sh
+++ setup-env.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+STATUS_SUCCESS=0
+STATUS_ERROR=1
+
 log(){
     echo " * $1"
 }
@@ -8,6 +11,21 @@ is_cmd_exists(){
     command -v "$1" > /dev/null 2>&1
     return $?
 }
+
+require_command () {
+    is_cmd_exists $1
+    cmd_exists=$?
+    if [ ! "$cmd_exists" -eq "$STATUS_SUCCESS" ] ;
+    then
+        log "the command '$1' not exists - please install manualy "
+        log " eg. # apt install $1"
+        log " then reran the script"
+        exit 1
+    else
+        return $STATUS_SUCCESS
+    fi
+}
+
 
 #
 # Just senety test
@@ -20,37 +38,45 @@ then
     exit 1
 fi
 
-is_cmd_exists "sudo"
-exists_sudo=$?
-if [ ! "$exists_sudo" -eq "0" ] ;
+
+#
+# Check requried
+#
+require_command "sudo"
+
+
+
+#
+# Check sudo
+#
+log "In order to install via 'apt install' command we should have sudo"
+log "you can cancel the installation via CTRL-C keys"
+sudo ls > /dev/null
+success_sudo=$?
+if [ ! "$success_sudo" -eq "0" ] ;
 then
-    log "sudo dose not exists - please install manualy"
-    exit 1
-else
-    echo "Will asking sudo for apt installs"
-    sudo ls > /dev/null
-    success_sudo=$?
-    if [ ! "$success_sudo" -eq "0" ] ;
-    then
-        echo "Without sudo we cannot install, bye-bye."
-        exit 20
-    fi
+    echo "Without sudo we cannot install, bye-bye."
+    exit 20
 fi
+
 exit 1
 
 
-is_cmd_exists "git" 
-git_exists=$?
-if [ "$git_exists" -eq "0" ] ;
-then
-    echo "Git exitst"
-else
- #   sudo apt install git
- echo "X"
-fi
-exit 0
+sudo apt install -y git wget ansible neovim neovim-qt \
+zsh zsh-autosuggetions zsh-syntax-highlighting \
+fzf ranger 
+
 #should have
 # GIT
 # Ansible
-
-ansible-playbook  --ask-become-pass site.yml
+# ansible-playbook  --ask-become-pass site.yml
+#
+#
+# Todo:
+# Dropbox
+# vim
+# nix
+# snap
+# fzf
+# ranger
+# kitty
